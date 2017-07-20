9379e74c0d6c8f1c30e8ff097e57e166a5931957
“NguyenLuc” <nguyenluc1232@gmail.com>
2017-07-20 12:55:57 +0700

    test

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -43,7 +43,7 @@ class MembersController extends Controller
     public function store(MemberRequest $request)
     {
         $input_data = $request->all();
-
+        $test;
         $member = new Member;
         $member->name = $input_data["name"];
         $member->address = $input_data["address"];
@@ -60,7 +60,10 @@ class MembersController extends Controller
         }
         $member->save();
         
-        return "Create member successfully";
+        return response()->json([
+            'error' => true,
+            'messages' => 'Create member successfully'
+        ]);
 
     }
 
